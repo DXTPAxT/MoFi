dc9a085399ebefe96a09313dfbfbd26153921fec
DXTPAxT <taidoyasuovn@gmail.com>
2024-05-09 19:19:48 +0700

    Hot fix

--- src/components/Layout/components/Sidebar2/MovieOptionContents/Genres/index.js
+++ src/components/Layout/components/Sidebar2/MovieOptionContents/Genres/index.js
@@ -5,7 +5,7 @@ import SlideShow from '~/components/Layout/components/SlideShow';
 const cx = classNames.bind(styles);
 
 function Genres() {
-    return <SlideShow></SlideShow>;
+    return;
 }
 
 export default Genres;
