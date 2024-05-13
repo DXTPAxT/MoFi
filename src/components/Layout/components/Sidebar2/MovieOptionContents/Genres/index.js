50f2af91b67ad9e36a1f1fb83840a8288c967278
DXTPAxT <taidoyasuovn@gmail.com>
2024-05-13 12:33:59 +0700

    Genres demo

--- src/components/Layout/components/Sidebar2/MovieOptionContents/Genres/index.js
+++ src/components/Layout/components/Sidebar2/MovieOptionContents/Genres/index.js
@@ -2,10 +2,39 @@ import styles from './Genres.module.scss';
 import classNames from 'classnames/bind';
 
 import SlideShow from '~/components/Layout/components/SlideShow';
+import MovieCard from '~/components/Layout/components/MovieCard';
+import demoThumb from '~/assets/img/genresDemo.jpg';
+import { useRef } from 'react';
+
 const cx = classNames.bind(styles);
 
 function Genres() {
-    return <div>Genres</div>;
+    const genres = [
+        { thumb: demoThumb, name: 'drama' },
+        { thumb: demoThumb, name: 'drama' },
+        { thumb: demoThumb, name: 'drama' },
+        { thumb: demoThumb, name: 'drama' },
+    ];
+
+    const genre = useRef();
+
+    return (
+        <div>
+            <SlideShow>
+                {genres.map((genre, i) => {
+                    return (
+                        <div ref={genre}>
+                            <MovieCard
+                                key={i}
+                                thumb={genre.demoThumb}
+                                name={genre.name}
+                            />
+                        </div>
+                    );
+                })}
+            </SlideShow>
+        </div>
+    );
 }
 
 export default Genres;
